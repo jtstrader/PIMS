37bc5581cb479655e629fae4a03546355479aea4
Jacob Strader <Jacob.Thomas.Strader@live.mercer.edu>
2022-04-27 07:37:42 -0400

    updated table component

--- pims-gui/src/app/table/table.component.ts
+++ pims-gui/src/app/table/table.component.ts
@@ -16,11 +16,23 @@ export class TableComponent implements OnInit {
   deathRates!: IDeathRatesByState[];
   errorMessage: string = "";
 
+  backId: number = 0;
+  bckgrdColors: string[] = ["#fff", "#ccc"];
+
   ngOnInit(): void {
     this.sub = this.deathRatesByState.getDeathRates().subscribe({
       next: drs => this.deathRates = drs,
       error: err => this.errorMessage = err
     });
+  }
+
+  alternateBackgroundColor(): string {
+    return this.bckgrdColors[this.backId == 0 ? this.backId++ : this.backId--]
+  }
+
+  // return a string to include trailing zeros
+  deathRateAdjust(death_rate: number): string {
+    return (death_rate * 100).toFixed(2);
   }
 
 }
