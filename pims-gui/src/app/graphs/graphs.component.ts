d4625bb66d08cdaf7e65f4bba140882e226abbd6
Sandgren-BenD <bsandgren726@gmail.com>
2022-05-01 19:01:41 -0400

    made 4 graphs with hardcoded data for now

--- pims-gui/src/app/graphs/graphs.component.ts
+++ pims-gui/src/app/graphs/graphs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { DeathRates, MaritalCounts, AverageSalary, PositionSalary } from './data';
 
 @Component({
   selector: 'app-graphs',
@@ -6,8 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graphs.component.css']
 })
 export class GraphsComponent implements OnInit {
-
-  constructor() { }
+  DeathRates!: any[];
+  MaritalCounts!: any[];
+  AverageSalary!: any[];
+  PositionSalary!: any[];
+  
+  
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = false;
+  showXAxisLabel = true;
+  showYAxisLabel = true;
+  timeline: boolean = true;
+  
+  
+  Graph1X = 'State';
+  Graph1Y = 'Death Rate';
+  
+  Graph2X = 'State';
+  Graph2Y = 'Marital Counts';
+  
+  Graph3X = 'Company';
+  Graph3Y = 'Average Salary';
+  
+  Graph4X = 'Company';
+  Graph4Y = 'Salary';
+  
+  constructor() { 
+    Object.assign(this, { DeathRates, MaritalCounts, AverageSalary, PositionSalary })
+  }
 
   ngOnInit(): void {
   }
